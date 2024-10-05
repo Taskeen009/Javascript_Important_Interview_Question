7ca4d53524413df2b596703862705f04b57231ff
Taskeen <taskeen.fa@example.com>
2024-10-05 13:43:39 +0530

    js objects

--- objects/index.js
+++ objects/index.js
@@ -23,4 +23,28 @@ console.log(Juser[mySym])
 
 Juser.name="leila"
 console.log(Juser.name)
-Object.freeze(Juser)
+//Object.freeze(Juser)
+
+
+//adding function they are trated jsut like variables
+Juser.greet=()=>{
+    return "hi"
+    //console.log("hi") y krne se last mai undefied bhi hai kyuki function kuch return nhi kr rha 
+}
+
+console.log(Juser.greet())
+
+
+// string interpolation
+
+Juser.Greet2=function (){
+    console.log(`hello ${this.name}`)
+}
+
+console.log(Juser.Greet2())
+
+// Juser.Greet2=()=>{
+//     console.log(`hello ${this.name}`)
+// }
+// console.log(Juser.Greet2())
+//this will not work because arrow function dont have this
